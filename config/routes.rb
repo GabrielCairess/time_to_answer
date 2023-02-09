894f1586deac0ecab43a371b595cbd88bcbfe6cc
Gabriel Caires <gbrlcaires@gmail.com>
2023-02-09 20:30:41 -0300

    adjust layouts backoffice admins and profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  namespace :admins_backoffice do
+    get 'welcome/index'
+  end
+
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+
   devise_for :admins
   devise_for :profiles
 end
